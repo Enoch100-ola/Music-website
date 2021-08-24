c6a94f86dfb507611167016e5e1114d99986db92
Enoch100-ola <enochola96@gmail.com>
2021-08-24 12:03:21 +0100

    Template set for work

---
+++ frontend/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from frontend import views
+
+app_name = 'frontend'
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('playlist/', views.playlist, name='playlist'),
+    path('team/', views.team , name= 'team'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('details/', views.details, name='details'),
+    path('tracks/', views.tracks, name='tracks'),
+    path('albums/', views.albums, name='albums')
+]
---
+++ frontend/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'frontend/index.html')
+
+def playlist(request):
+    return render (request, 'frontend/playlist.html')
+
+def team(request):
+    return render(request, 'frontend/team.html')
+
+def contact(request):
+    return render(request, 'frontend/contact-us.html')
+
+def login(request):
+    return render(request, 'frontend/login.html')
+
+def register(request):
+    return render(request, 'frontend/register.html')
+
+def details(request):
+    return render(request, 'frontend/details.html')
+
+def tracks(request):
+    return render(request, 'frontend/tracks.html')
+
+def albums(request):
+    return render(request, 'frontend/album.html')
+
+
